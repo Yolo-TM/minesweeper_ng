fd8a3b2e5daf335d1e52c345e95a68982493d5d8
Yolo <timm.sauer@outlook.de>
2025-04-16 11:10:51 +0200

    inserted evil ng field

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,8 @@ fn main() {
     let field2 = MineSweeperField::new_percentage(45, 26, 0.22);
     field2.println();
     minesweeper_solver(field2);
+
+    let ng_field = get_ng_minesweeper_field();
+    ng_field.println();
+    minesweeper_solver(ng_field);
 }
