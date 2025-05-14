2e8ca03bfa4fa88626e8eb991d63a5e6475d38b1
Yolo <timm.sauer@outlook.de>
2025-05-14 11:59:50 +0200

    notes

--- src/ng_generator/minesweeper_ng_field.rs
+++ src/ng_generator/minesweeper_ng_field.rs
@@ -1,1 +1,8 @@
 // Create the NG Minesweeper Field
+
+/*
+if not solvable, check if there are multiple islands
+    if yes, take a random mine from an island border and move it to another island border
+        then try solving again? possible from the current sovler state theoretically
+    if no, check minecount, unrevealed etc if it makes sense to just move the mine somewhere else (from the border away)
+*/
