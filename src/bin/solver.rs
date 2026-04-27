e1c8bf5b8fa3c5c1e0eaf3762e9c7797093cc714
Yolo-TM <yolo.tm@outlook.de>
2026-04-27 16:49:09 +0200

    removed old solver binary

--- src/bin/solver.rs
+++
@@ -1,8 +0,0 @@
-use minesweeper_ng_gen::*;
-
-fn main() {
-    //let field = get_evil_ng_field();
-    let field: RandomField = RandomField::new(60, 40, Mines::Density(0.20)).unwrap();
-    field.show();
-    solve_field(&field);
-}
